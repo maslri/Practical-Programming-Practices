e5f3371303bac194f6017c3494f9496f6297a730
maslri <ma.slri2128@gmail.com>
2023-08-03 17:20:10 +0330

    I changed the Code by My Idea.

--- strings/tfcctf2023-dizzy/README.md
+++ strings/tfcctf2023-dizzy/README.md
@@ -7,3 +7,4 @@ Embark on 'Dizzy', a carousel ride through cryptography! This warmup challenge s
 '''
 T4 l16 _36 510 _27 s26 _11 320 414 {6 }39 C2 T0 m28 317 y35 d31 F1 m22 g19 d38 z34 423 l15 329 c12 ;37 19 h13 _30 F5 t7 C3 325 z33 _21 h8 n18 132 k24
 '''
+### *** It conveys the idea that the number of empty spaces at the beginning of the project does not need to be fixed manually, as the project itself determines the required number based on the input.
--- strings/tfcctf2023-dizzy/solve.py
+++ strings/tfcctf2023-dizzy/solve.py
@@ -1,18 +1,20 @@
-string = "T4 l16 _36 510 _27 s26 _11 320 414 {6 }39 C2 T0 m28 317 y35 d31 F1 m22 g19 d38 z34 423 l15 329 c12 ;37 19 h13 _30 F5 t7 C3 325 z33 _21 h8 n18 132 k24"
+''' Embark on 'Dizzy', a carousel ride through cryptography! 
+    This warmup challenge spins you around the basics of ciphers and keys. 
+    Sharpen your mind, find the flag, and remember - in crypto, it's fun to get a little dizzy! '''
 
-words = string.split(' ')
+''' T4 l16 _36 510 _27 s26 _11 320 414 {6 }39 
+    C2 T0 m28 317 y35 d31 F1 m22 g19 d38 z34 423 
+    l15 329 c12 ;37 19 h13 _30 F5 t7 C3 325 z33 _21 h8 n18 132 k24 '''
 
-answer = []
-for i in range(100):
-    answer.append('*')
+input = '''T4 l16 _36 510 _27 s26 _11 320 414 {6 }39 C2 T0 m28 317 y35 d31 F1 m22 g19 d38 z34 423 l15 329 c12 ;37 19 h13 _30 F5 t7 C3 325 z33 _21 h8 n18 132 k24'''
+result = [None]
 
-def print_from_array(answer):
-    print (''.join(answer))
+str_list = input.split(" ")
+for x in str_list:
+    index = int(x[1:])
+    value = x[0]
+    if ( index > len(result)) :
+        result.extend([None] * (index - len(result) + 1))
+    result[index] = value
 
-for w in words:
-    c = w[0]
-    i = int(w[1:])
-    answer[i] = c
-
-print("found the answer!")
-print_from_array(answer)
+print(''.join(result))
